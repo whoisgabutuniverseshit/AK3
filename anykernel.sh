e87041f19cc679f53e9e70bf91e3d44208d87f0b
fjrXTR <fajarslebew31@gmail.com>
2024-07-08 12:46:38 +0000

    AK3: Rebranding
    Change-Id: Ic131b2a41d002efc09024d0a0dc2b5122170864a
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,18 +4,18 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=QuicksilveR Kernel by ghostrider-reborn
+kernel.string=villhaze! kernel by fjrXTR 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
+device.name1=ruby
+device.name2=rubypro
 device.name3=
 device.name4=
 device.name5=
-supported.versions=11.0-13.0
+supported.versions=12.0-15.0
 supported.patchlevels=
 '; } # end properties
 
@@ -28,8 +28,8 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## boot shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+block=/dev/block/by-name/boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
 
